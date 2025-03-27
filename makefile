3461b1670fcf7a1b250670fd940bbea21212d8a6
Marcin <marcin.konow@lczyk.xyz>
2025-03-27 18:04:02 +0000

    print env

--- makefile
+++ makefile
@@ -12,15 +12,15 @@ run: ./shim/main
 	./shim/main
 
 
-install: myshim
+install: ./shim/main-arm64
 	chmod +x ./scripts/macos_docker_desktop_hyperv_login.sh
-	./scripts/macos_docker_desktop_hyperv_login.sh -nvk -f /shim/main-arm64:foo/bar/shim
+	./scripts/macos_docker_desktop_hyperv_login.sh -nvk -f ./shim/main-arm64:foo/bar/shim
 
 # for now we hack the shim uninstall by tunnelling into the hypervisor without the -k flag so that the
 # shim is deleted after exit
 uninstall:
 	chmod +x ./scripts/macos_docker_desktop_hyperv_login.sh
-	./scripts/macos_docker_desktop_hyperv_login.sh -nv -f /shim/main-arm64:foo/bar/shim
+	./scripts/macos_docker_desktop_hyperv_login.sh -nv -f ./shim/main-arm64:foo/bar/shim
 
 shimmed-alpine:
 	docker run --runtime foo -it --rm alpine:latest
--- shim/main.go
+++ shim/main.go
@@ -8,8 +8,6 @@ import (
 )
 
 func main() {
-	// get all commandline arguments
-	fmt.Println("os.Args:", os.Args)
 	// figure out where the shim is located
 	path, err := filepath.Abs(os.Args[0])
 	if err != nil {
@@ -17,6 +15,7 @@ func main() {
 		return
 	}
 	log_file := filepath.Join(filepath.Dir(path), "shim.log")
+
 	// write arguments to the log file
 	f, err := os.OpenFile(log_file, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
@@ -25,11 +24,41 @@ func main() {
 	defer f.Close()
 
 	now := time.Now()
-	line := fmt.Sprintf("os.Args: %v\n", os.Args)
 
-	_, err = f.WriteString(fmt.Sprintf("%s: %s", now.Format(time.RFC3339), line))
+	log := func(line string) {
+		_, err = f.WriteString(fmt.Sprintf("%s: %s", now.Format(time.RFC3339), line))
+		if err != nil {
+			fmt.Println("Error:", err)
+			return
+		}
+	}
+
+	log(fmt.Sprintf("args: %v\n", os.Args))
+
+	// Get all the environment variables
+	env := os.Environ()
+
+	// write environment variables to the log file
+	for _, e := range env {
+		log(fmt.Sprintf("env: %s\n", e))
+	}
+
+	// write the current working directory to the log file
+
+	cwd, err := os.Getwd()
 	if err != nil {
-		fmt.Println("Error:", err)
+		log(fmt.Sprintf("Error: %s\n", err))
 		return
 	}
+	log(fmt.Sprintf("cwd: %s\n", cwd))
+
+	// write the current user to the log file
+
+	user, err := os.UserHomeDir()
+	if err != nil {
+		log(fmt.Sprintf("Error: %s\n", err))
+		return
+	}
+
+	log(fmt.Sprintf("user: %s\n", user))
 }
